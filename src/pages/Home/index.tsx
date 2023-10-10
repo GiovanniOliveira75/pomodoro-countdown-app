59addd815d0e089e6ea8581557799e94beaf92f8
GiovanniOliveira <giovanni-g@hotmail.com>
2023-10-09 21:41:36 -0300

    fix: components path casing

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,8 +9,8 @@ import {
   StartCountdownButton,
   StopCountdownButton,
 } from './styles'
-import { NewCycleForm } from './components/NewCycleForm'
-import { Countdown } from './components/Countdown'
+import { NewCycleForm } from './Components/NewCycleForm'
+import { Countdown } from './Components/Countdown'
 import { CyclesContext } from '../../contexts/CyclesContext'
 
 const newCycleFormValidationSchema = zod.object({
